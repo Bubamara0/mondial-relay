ded14a356371f1b6d599d683875d4453e975481e
Romain <romain.c@institutsolacroup.com>
2022-01-10 13:56:30 +0100

    Updated homepage

--- src/server.js
+++ src/server.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 8080;
+const Joi = require("joi");
 
 // ---------------------------------------------------------------------------
 // Faites "npm i" pour installer tous les modules utilisés dans l'application
 // ---------------------------------------------------------------------------
 
 app.get("/", (req, res) => {
-	res.send("Our dear homepage");
+	// res.send() temporaire :
+	res.send(`
+		<h1>Our dear homepage</h1>
+		<ul>
+			<li><a href="http://api.mondialrelay.com/Web_Services.asmx" target="__blank">Mondial Relay - WSI2_RechercheCP</a></li>
+			<li><a href="https://www.mondialrelay.fr/media/122867/solution-web-service-v57.pdf" target="__blank">Mondial Relay - Web Service PDF</a></li>
+		</ul>
+	`);
 });
 
 app.listen(PORT, () => {
