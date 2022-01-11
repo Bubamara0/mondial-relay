eb0220bc849b2d8c1d18167134e386a418d46175
Mobzaaz <mobzaaz69@gmail.com>
2022-01-11 16:05:53 +0100

    first commit

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const PORT = process.env.PORT || 8080;
+const Joi = require("joi");
+const bodyParser = require("body-parser");
+
+
+const axios = require("axios");
 
 // ---------------------------------------------------------------------------
 // Faites "npm i" pour installer tous les modules utilisés dans l'application
@@ -11,6 +15,56 @@ app.get("/", (req, res) => {
 	res.send("Our dear homepage");
 });
 
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({
+	extended: true
+  }));
+
+app.post("/prsearch", (req, res) => {
+	
+	console.log(req.query.nbresults)
+
+	//1: Récupérer et valider le body (requête JSON)
+
+	const body = req.body
+
+	console.log(body)
+
+	const schema = Joi.object({
+		Pays: Joi.string()
+			.min(2)
+			.max(2)
+			.required(),
+		CP: Joi.number().required()
+	})
+
+	const { error } = schema.validate(body);
+
+	if(error) {
+	  res.status(400).send(`Bad Request!\n${error.details[0].message}`)
+	}
+
+	res.status(200).send("Good Request!")
+
+	//2: Construire la requête : headers et body a préparer
+
+	const { data } = await axios.post("http://api.mondialrelay.com/Web_Services.asmx?op=WSI4_PointRelais_Recherche", body, {
+		headers: {
+			'content-type': 'text/xml'
+		}
+	})
+
+	//3: Envoyer la requête et observer la réponse
+	//4: Extraire les infos de la réponse
+	//5: Construire la réponse (JSON)
+	//6: Envoyer la réponse
+})
+
+
+const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
 	console.log(`Server started on http://localhost:${PORT}`);
 });
+
