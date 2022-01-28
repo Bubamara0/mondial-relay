c78fbf57754835c8153b5724e4666e4992042759
Daymortel <daymortel@gmail.com>
2022-01-28 15:28:38 +0100

    encore moi

--- frontend/dist/index.b9efb4a5.js
+++
@@ -1,8 +0,0 @@
-document.querySelector("#navbar .burger").addEventListener("click", ()=>{
-    document.querySelector("#overlay").style.setProperty("display", "flex");
-});
-document.querySelector("#overlay .cross").addEventListener("click", ()=>{
-    document.querySelector("#overlay").style.setProperty("display", "none");
-});
-
-//# sourceMappingURL=index.b9efb4a5.js.map
--- frontend/public/js/backRequest.js
+++ frontend/public/js/backRequest.js
@@ -1,41 +1,45 @@
 const PORT = 8080;
-// const axios = require("axios")
 import axios from "axios"
+import { object } from "joi";
 
-document.querySelector("form").addEventListener("submit", (e) => {
+document.querySelector("form").addEventListener("submit", async (e) => {
+
 	e.preventDefault()
-	axios.post(`http://localhost:${PORT}/creationEtiquette`, {
-		params : {
-			ModeCol : document.getElementsByName("ModeCol").value,
-			ModeLiv : document.getElementsByName("ModeLiv").value,
-			Langage_expediteur : document.getElementsByName("Langage_expediteur").value,
-			Nom_expediteur : document.getElementsByName("Nom_expediteur").value,
-			Rue_expediteur : document.getElementsByName("Rue_expediteur").value,
-			Ville_expediteur : document.getElementsByName("Ville_expediteur").value,
-			Code_postal_expediteur : document.getElementsByName("Code_postal_expediteur").value,
-			Pays_expediteur : document.getElementsByName("Pays_expediteur").value,
-			Telephone_expediteur : document.getElementsByName("Telephone_expediteur").value,
-			Mail_expediteur : document.getElementsByName("Mail_expediteur").value,
-			Langage_destinataire : document.getElementsByName("Langage_destinataire").value,
-			Nom_destinataire : document.getElementsByName("Nom_destinataire").value,
-			Rue_destinataire : document.getElementsByName("Rue_destinataire").value,
-			Ville_destinataire : document.getElementsByName("Ville_destinataire").value,
-			Code_postal_destinataire : document.getElementsByName("Code_postal_destinataire").value,
-			Pays_destinataire : document.getElementsByName("Pays_destinataire").value,
-			Telephone_destinataire : document.getElementsByName("Telephone_destinataire").value,
-			Mail_destinataire : document.getElementsByName("Mail_destinataire").value,
-			Poids : document.getElementsByName("Poids").value,
-			Nombre_colis : document.getElementsByName("Nombre_colis").value,
-			Montant_contre_remboursement : document.getElementsByName("Montant_contre_remboursement").value,
-			COL_Rel_Pays : document.getElementsByName("COL_Rel_Pays").value,
-			COL_Rel : document.getElementsByName("COL_Rel").value,
-			LIV_Rel_Pays : document.getElementsByName("LIV_Rel_Pays").value,
-			LIV_Rel : document.getElementsByName("LIV_Rel").value
-		}
-	}
+    try {
+	await axios.post(`http://localhost:${PORT}/creationEtiquette`, {
+		ModeCol : document.getElementsByName("ModeCol")[0].value,
+        ModeLiv : document.getElementsByName("ModeLiv")[0].value,
+        Langage_expediteur : document.getElementsByName("Langage_expediteur")[0].value,
+        Nom_expediteur : document.getElementsByName("Nom_expediteur")[0].value,
+        Rue_expediteur : document.getElementsByName("Rue_expediteur")[0].value,
+        Ville_expediteur : document.getElementsByName("Ville_expediteur")[0].value,
+        Code_postal_expediteur : document.getElementsByName("Code_postal_expediteur")[0].value,
+        Pays_expediteur : document.getElementsByName("Pays_expediteur")[0].value,
+        Telephone_expediteur : document.getElementsByName("Telephone_expediteur")[0].value,
+        Mail_expediteur : document.getElementsByName("Mail_expediteur")[0].value,
+        Langage_destinataire : document.getElementsByName("Langage_destinataire")[0].value,
+        Nom_destinataire : document.getElementsByName("Nom_destinataire")[0].value,
+        Rue_destinataire : document.getElementsByName("Rue_destinataire")[0].value,
+        Ville_destinataire : document.getElementsByName("Ville_destinataire")[0].value,
+        Code_postal_destinataire : document.getElementsByName("Code_postal_destinataire")[0].value,
+        Pays_destinataire : document.getElementsByName("Pays_destinataire")[0].value,
+        Telephone_destinataire : document.getElementsByName("Telephone_destinataire")[0].value,
+        Mail_destinataire : document.getElementsByName("Mail_destinataire")[0].value,
+        Poids : document.getElementsByName("Poids")[0].value,
+        Nombre_colis : document.getElementsByName("Nombre_colis")[0].value,
+        Montant_contre_remboursement : document.getElementsByName("Montant_contre_remboursement")[0].value,
+        COL_Rel_Pays : document.getElementsByName("COL_Rel_Pays")[0].value,
+        COL_Rel : document.getElementsByName("COL_Rel")[0].value,
+        LIV_Rel_Pays : document.getElementsByName("LIV_Rel_Pays")[0].value,
+        LIV_Rel : document.getElementsByName("LIV_Rel")[0].value
+	}).then(({ data }) => {
+        window.location.href = data
+    })
 	
-).then(({ data }) => {
-		console.log(data)
-	})
-	console.log(document.getElementsByName("ModeCol"));
+ }catch(err) {
+     if(err) {
+        console.log(err.message)
+     }
+ }
+
 })
