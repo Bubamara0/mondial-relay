b1992aaa5bf51ad826aba92fd0b2886b81127772
Romain <romain.c@institutsolacroup.com>
2022-01-28 14:50:33 +0100

    Updated front request

--- frontend/public/js/fillForm.js
+++ frontend/public/js/fillForm.js
@@ -2,8 +2,8 @@ document.getElementsByName("ModeCol")[0].value = "CCC";
 document.getElementsByName("ModeLiv")[0].value = "24H";
 
 document.getElementsByName("Langage_expediteur")[0].value = "FR";
-document.getElementsByName("Nom_expediteur")[0].value = "Harry Kovert";
-document.getElementsByName("Rue_expediteur")[0].value = "7, rue du petit pois";
+document.getElementsByName("Nom_expediteur")[0].value = "MR Harry Kovert";
+document.getElementsByName("Rue_expediteur")[0].value = "Rue_du_petit_pois,_7";
 document.getElementsByName("Ville_expediteur")[0].value = "Dinard";
 document.getElementsByName("Code_postal_expediteur")[0].value = "35800";
 document.getElementsByName("Pays_expediteur")[0].value = "FR";
@@ -11,8 +11,8 @@ document.getElementsByName("Telephone_expediteur")[0].value = "0123456789";
 document.getElementsByName("Mail_expediteur")[0].value = "xX_harrykovert_Xx@gmail.com";
 
 document.getElementsByName("Langage_destinataire")[0].value = "FR";
-document.getElementsByName("Nom_destinataire")[0].value = "Tom Âte";
-document.getElementsByName("Rue_destinataire")[0].value = "2, rue du gaspacho";
+document.getElementsByName("Nom_destinataire")[0].value = "MR Tom Âte";
+document.getElementsByName("Rue_destinataire")[0].value = "Rue_du_gaspacho,_2";
 document.getElementsByName("Ville_destinataire")[0].value = "Dinard";
 document.getElementsByName("Code_postal_destinataire")[0].value = "35800";
 document.getElementsByName("Pays_destinataire")[0].value = "FR";
