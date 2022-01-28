146f65e3e945df7a4d74a4932d7bf44b17c0744e
Romain <romain.c@institutsolacroup.com>
2022-01-28 13:39:48 +0100

    Added auto form filler

---
+++ frontend/public/js/fillForm.js
@@ -0,0 +1,28 @@
+document.getElementsByName("ModeCol")[0].value = "CCC";
+document.getElementsByName("ModeLiv")[0].value = "24H";
+
+document.getElementsByName("Langage_expediteur")[0].value = "FR";
+document.getElementsByName("Nom_expediteur")[0].value = "Jean-Jacques Panzani";
+document.getElementsByName("Rue_expediteur")[0].value = "7 rue du spaghetti cru";
+document.getElementsByName("Ville_expediteur")[0].value = "Dinard";
+document.getElementsByName("Code_postal_expediteur")[0].value = "35800";
+document.getElementsByName("Pays_expediteur")[0].value = "FR";
+document.getElementsByName("Telephone_expediteur")[0].value = "0123456789";
+document.getElementsByName("Mail_expediteur")[0].value = "test@gmail.com";
+
+document.getElementsByName("Langage_destinataire")[0].value = "FR";
+document.getElementsByName("Nom_destinataire")[0].value = "Jean-Jacques Panzani";
+document.getElementsByName("Rue_destinataire")[0].value = "7 rue du spaghetti cru";
+document.getElementsByName("Ville_destinataire")[0].value = "Dinard";
+document.getElementsByName("Code_postal_destinataire")[0].value = "35800";
+document.getElementsByName("Pays_destinataire")[0].value = "FR";
+document.getElementsByName("Telephone_destinataire")[0].value = "0123456789";
+document.getElementsByName("Mail_destinataire")[0].value = "test@gmail.com";
+
+document.getElementsByName("Poids")[0].value = "20";
+document.getElementsByName("Nombre_colis")[0].value = "1";
+document.getElementsByName("Montant_contre_remboursement")[0].value = "50";
+document.getElementsByName("COL_Rel_Pays")[0].value = "24H";
+document.getElementsByName("COL_Rel")[0].value = "24H";
+document.getElementsByName("LIV_Rel_Pays")[0].value = "24H";
+document.getElementsByName("LIV_Rel")[0].value = "24H";
