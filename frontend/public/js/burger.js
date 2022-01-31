871734bb858a0485cf543b183cce9e37faddc422
Romain <romain.c@institutsolacroup.com>
2022-01-31 11:37:32 +0100

    Updated files

--- frontend/public/js/burger.js
+++ frontend/public/js/burger.js
@@ -1,7 +1,16 @@
-document.querySelector("#navbar .burger").addEventListener("click", () => {
+const showOverlay = () => {
+    document.querySelector("#navbar .burger").style.setProperty("display", "none");
+    document.querySelector("#overlay .cross").style.setProperty("display", "flex");
     document.querySelector("#overlay").style.setProperty("display", "flex");
-});
+}
 
-document.querySelector("#overlay .cross").addEventListener("click", () => {
+const hideOverlay = () => {
+    document.querySelector("#navbar .burger").style.setProperty("display", "flex");
+    document.querySelector("#overlay .cross").style.setProperty("display", "none");
     document.querySelector("#overlay").style.setProperty("display", "none");
-});
+}
+
+document.querySelector("#navbar .burger").addEventListener("click", showOverlay);
+
+document.querySelector("#overlay").addEventListener("click", hideOverlay);
+document.querySelector("#overlay .cross").addEventListener("click", hideOverlay);
